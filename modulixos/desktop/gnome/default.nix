317fc6d8a86d6adfc19c31f753c106056c55d64d
qhorgues <quentin.horgues@outlook.fr>
2026-09-01 14:44:27 +0200

    [feature] set gnome as default session

--- modulixos/desktop/gnome/default.nix
+++ modulixos/desktop/gnome/default.nix
@@ -37,6 +37,7 @@ in
         lib.mkIf deEnabled {
           services = {
             displayManager.gdm.enable = true;
+            displayManager.defaultSession = lib.mkDefault "gnome";
             desktopManager.gnome = {
               sessionPath = lib.optional cfg.rounded-blur gnome-rounded-blur;
               enable = true;
