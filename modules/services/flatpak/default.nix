4f015cd444cc5f3f23654b01e8417d699ef24454
qhorgues <quentin.horgues@outlook.fr>
2026-09-12 21:33:34 +0200

    fix flatpak path

--- modules/services/flatpak/default.nix
+++ modules/services/flatpak/default.nix
@@ -11,6 +11,10 @@ in
   config = lib.mkIf cfg.enable {
     services.flatpak.enable = true;
 
+    environment.sessionVariables.XDG_DATA_DIRS = [
+      "/var/lib/flatpak/exports/share"
+    ];
+
     systemd.services.flatpak-repo = {
       wantedBy = [ "multi-user.target" ];
       path = [ pkgs.flatpak ];
